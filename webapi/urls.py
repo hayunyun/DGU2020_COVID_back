bc35b04400e26173ed20b2d8ffc0f9d411f0434e
Sungmin Woo <woos8899@gmail.com>
2020-10-16 06:42:48 +0900

    Webapi to fetch sequence metadata

--- webapi/urls.py
+++ webapi/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     dur.path('testlist/', views.TestList.as_view()),
     dur.path("echo/", views.Echo.as_view()),
+    dur.path("similar_seq_ids/", views.SimilarSeqIDs.as_view()),
 ]
