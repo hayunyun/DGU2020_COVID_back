a6a861c45bd4dee9f63184583c097261fdea6096
Sungmin Woo <woos8899@gmail.com>
2020-10-23 02:42:52 +0900

    An api function that only asks for sequence id list

--- webapi/urls.py
+++ webapi/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     dur.path('testlist/', views.TestList.as_view()),
     dur.path("echo/", views.Echo.as_view()),
     dur.path("similar_seq_ids/", views.SimilarSeqIDs.as_view()),
+    dur.path("get_similar_seq_ids/", views.GetSimilarSeqIDs.as_view())
 ]
--- webapi/views.py
+++ webapi/views.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 from typing import Dict
 
 from rest_framework.views import APIView
@@ -12,6 +13,15 @@ import dgucovidb.sql_interface as sql
 # This path is relative to the repo root path where manage.py is located
 BLAST_INTERF = bst.InterfBLAST("./database/blast")
 MYSQL_INTERF = sql.InterfMySQL("dgucovid", "COVID@dgu2020")
+
+KEY_SEQ = "seq"
+KEY_SEQ_ID = "seq_id"
+KEY_SEQ_ID_LIST = "seq_id_list"
+
+KEY_HOW_MANY = "how_many"
+
+KEY_ERROR_CODE = "error_code"
+KEY_ERROR_TEXT = "error_text"
 
 
 class TestList(APIView):
@@ -58,3 +68,88 @@ class SimilarSeqIDs(APIView):
             result[acc_id] = MYSQL_INTERF.get_metadata_of(acc_id)
 
         return Response(result)
+
+
+class GetSimilarSeqIDs(APIView):
+    """
+    Requst payload must have following fields
+    * seq: string -> A DNA sequence of covid19
+    * how_many: number ->
+
+    On success, it responds with following fields
+    * seq_id_list: array[string] -> List of sequence IDs which represent sequences that are similar to input sequence by client
+    * error_code: number -> It should be 0
+
+    Meanwhile on failure, the reponse payload contains followings
+    * error_code: number -> It can be any integer number but 0
+    * error_text: string -> Refer to local variable "ERROR_MAP" for details
+    """
+    def post(self, request: Request, format_arg=None):
+        ERROR_MAP = {
+            1: "Unkown error",
+            2: "Invalid request payload",
+            3: "Key '{}' not found",
+            4: "Expected '{}' to be a '{}', got '{}' instead",  # Wrong type for a value
+            5: "Failed to generate BLAST query result",
+        }
+
+        try:
+            #### Validate client input ####
+
+            if not isinstance(request.data, dict):
+                return Response({KEY_ERROR_CODE: 2, KEY_ERROR_TEXT: ERROR_MAP[2]})
+
+            payload: dict = request.data
+            if KEY_SEQ not in payload.keys():
+                return Response({
+                    KEY_ERROR_CODE: 3,
+                    KEY_ERROR_TEXT: ERROR_MAP[3].format(KEY_SEQ)
+                })
+            if KEY_HOW_MANY not in payload.keys():
+                return Response({
+                    KEY_ERROR_CODE: 3,
+                    KEY_ERROR_TEXT: ERROR_MAP[3].format(KEY_HOW_MANY)
+                })
+
+            maybe_seq = payload[KEY_SEQ]
+            try:
+                seq = str(maybe_seq)
+            except:
+                return Response({
+                    KEY_ERROR_CODE: 4,
+                    KEY_ERROR_TEXT: ERROR_MAP[4].format(KEY_SEQ, "str", type(maybe_seq))
+                })
+
+            maybe_how_many = payload[KEY_HOW_MANY]
+            try:
+                how_many = int(maybe_how_many)
+            except:
+                return Response({
+                    KEY_ERROR_CODE: 4,
+                    KEY_ERROR_TEXT: ERROR_MAP[4].format(KEY_HOW_MANY, "int", type(maybe_how_many))
+                })
+
+            #### Work ####
+
+            result_file_name = "result.tmp"
+            print("[] started blast query")
+            if not BLAST_INTERF.gen_query_result(seq, result_file_name):
+                os.remove(result_file_name)
+                return Response({KEY_ERROR_CODE: 5, KEY_ERROR_TEXT: ERROR_MAP[5]})
+            print("[] finished blast query")
+
+            ids = BLAST_INTERF.find_ids_of_the_similars(result_file_name, how_many)
+            print("[] ids foind: {}".format(ids))
+            os.remove(result_file_name)
+
+            return Response({
+                KEY_SEQ_ID_LIST: ids,
+                KEY_ERROR_CODE: 0,
+            })
+
+        except:
+            traceback.print_exc()
+            return Response({
+                KEY_ERROR_CODE: 1,
+                KEY_ERROR_TEXT: ERROR_MAP[1],
+            })
