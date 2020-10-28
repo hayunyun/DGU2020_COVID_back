0e19add9b476c931f853b46d5e4730b0339072ad
Sungmin Woo <woos8899@gmail.com>
2020-10-28 23:42:05 +0900

    User request input validation as a function

--- webapi/views.py
+++ webapi/views.py
@@ -1,10 +1,11 @@
 import os
 import traceback
-from typing import Dict
+from typing import Dict, Type, Optional
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.exceptions import ParseError
 
 import dgucovidb.db_interface as bst
 import dgucovidb.sql_interface as sql
@@ -14,6 +15,74 @@ from . import konst as cst
 
 BLAST_INTERF = bst.InterfBLAST(cst.BLAST_DB_PATH)
 MYSQL_INTERF = sql.InterfMySQL(cst.MYSQL_USERNAME, cst.MYSQL_PASSWORD)
+
+
+class ErrorMap:
+    def __init__(self):
+        self.__map: Dict[int, str] = {}
+
+        self.add_spec_dict({
+            1: "Unkown error",
+            2: "Invalid request payload",
+            3: "Key '{}' not found",
+            4: "Expected '{}' to be a '{}', got '{}' instead",  # Wrong type for a value
+            5: "Failed to generate BLAST query result",
+            6: "Invalid json syntax",
+        })
+
+    def __getitem__(self, err_code: int):
+        return self.get_message(err_code)
+
+    def add_spec(self, err_code: int, err_message: str):
+        assert isinstance(err_code, int)
+        assert isinstance(err_message, str)
+
+        if err_code in self.__map.keys():
+            raise RuntimeError("Tried to add a key which was already registered")
+
+        self.__map[err_code] = err_message
+
+    def add_spec_dict(self, specs: Dict[int, str]):
+        for k, v in specs.items():
+            self.add_spec(k, v)
+
+    def get_message(self, err_code: int):
+        return self.__map[err_code]
+
+
+ERROR_MAP = ErrorMap()
+
+
+# Returns None if the payload is valid
+def _validate_request_payload(req: Request, criteria: Dict[str, Type]) -> Optional[dict]:
+    try:
+        payload = req.data
+    except ParseError:
+        return {
+            cst.KEY_ERROR_CODE: 6,
+            cst.KEY_ERROR_TEXT: ERROR_MAP[6]
+        }
+
+    if not isinstance(payload, dict):
+        return {cst.KEY_ERROR_CODE: 2, cst.KEY_ERROR_TEXT: ERROR_MAP[2]}
+
+    for key_name, value_type in criteria.items():
+        if key_name not in payload.keys():
+            return {
+                cst.KEY_ERROR_CODE: 3,
+                cst.KEY_ERROR_TEXT: ERROR_MAP[3].format(key_name)
+            }
+
+        maybe_value = payload[key_name]
+        if not isinstance(maybe_value, value_type):
+            return {
+                cst.KEY_ERROR_CODE: 4,
+                cst.KEY_ERROR_TEXT: ERROR_MAP[4].format(
+                    cst.KEY_HOW_MANY, value_type.__name__, type(maybe_value).__name__
+                )
+            }
+
+    return None
 
 
 class Echo(APIView):
@@ -76,49 +145,18 @@ class GetSimilarSeqIDs(APIView):
 
     @staticmethod
     def post(request: Request, format_arg=None):
-        ERROR_MAP = {
-            1: "Unkown error",
-            2: "Invalid request payload",
-            3: "Key '{}' not found",
-            4: "Expected '{}' to be a '{}', got '{}' instead",  # Wrong type for a value
-            5: "Failed to generate BLAST query result",
-        }
-
         try:
             #### Validate client input ####
 
-            if not isinstance(request.data, dict):
-                return Response({cst.KEY_ERROR_CODE: 2, cst.KEY_ERROR_TEXT: ERROR_MAP[2]})
+            validate_result = _validate_request_payload(request, {
+                cst.KEY_SEQUENCE: str,
+                cst.KEY_HOW_MANY: int,
+            })
+            if validate_result is not None:
+                return Response(validate_result)
 
-            payload: dict = request.data
-            if cst.KEY_SEQUENCE not in payload.keys():
-                return Response({
-                    cst.KEY_ERROR_CODE: 3,
-                    cst.KEY_ERROR_TEXT: ERROR_MAP[3].format(cst.KEY_SEQUENCE)
-                })
-            if cst.KEY_HOW_MANY not in payload.keys():
-                return Response({
-                    cst.KEY_ERROR_CODE: 3,
-                    cst.KEY_ERROR_TEXT: ERROR_MAP[3].format(cst.KEY_HOW_MANY)
-                })
-
-            maybe_seq = payload[cst.KEY_SEQUENCE]
-            try:
-                seq = str(maybe_seq)
-            except:
-                return Response({
-                    cst.KEY_ERROR_CODE: 4,
-                    cst.KEY_ERROR_TEXT: ERROR_MAP[4].format(cst.KEY_SEQUENCE, "str", type(maybe_seq))
-                })
-
-            maybe_how_many = payload[cst.KEY_HOW_MANY]
-            try:
-                how_many = int(maybe_how_many)
-            except:
-                return Response({
-                    cst.KEY_ERROR_CODE: 4,
-                    cst.KEY_ERROR_TEXT: ERROR_MAP[4].format(cst.KEY_HOW_MANY, "int", type(maybe_how_many))
-                })
+            seq = str(request.data[cst.KEY_SEQUENCE])
+            how_many = int(request.data[cst.KEY_HOW_MANY])
 
             #### Work ####
 
