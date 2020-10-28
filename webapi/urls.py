93adb1b6d809343f914d6387252686be38925af8
Sungmin Woo <woos8899@gmail.com>
2020-10-29 00:36:55 +0900

    Removed an api url

--- webapi/urls.py
+++ webapi/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     dur.path("echo/", views.Echo.as_view()),
-    dur.path("similar_seq_ids/", views.SimilarSeqIDs.as_view()),
     dur.path("get_similar_seq_ids/", views.GetSimilarSeqIDs.as_view()),
     dur.path("get_metadata_of_seq/", views.GetMetadataOfSeq.as_view()),
 ]
