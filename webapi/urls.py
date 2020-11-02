148b50b6394d6f6fe63e530cbd5fdd99aaf66dcd
hayunyun <hayunyun@naver.com>
2020-11-03 05:39:44 +0900

    add

--- webapi/urls.py
+++ webapi/urls.py
@@ -1,22 +1,11 @@
-from django.urls import path
+import django.urls as dur
 from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-    path('testlist/', views.TestList.as_view()),
-    path("echo/", views.Echo.as_view()),
-    path("get_similar_seq_ids/", views.SimilarSeqIDs.as_view()),
-=======
-=======
->>>>>>> 171a8c111c225ded5026786f103771f2c0a174cf
     dur.path("echo/", views.Echo.as_view()),
     dur.path("get_similar_seq_ids/", views.GetSimilarSeqIDs.as_view()),
     dur.path("get_metadata_of_seq/", views.GetMetadataOfSeq.as_view()),
     dur.path("calc_similarity_of_two_seq/", views.CalcSimilarityOfTwoSeq.as_view()),
-<<<<<<< HEAD
->>>>>>> 171a8c111c225ded5026786f103771f2c0a174cf
-=======
->>>>>>> 171a8c111c225ded5026786f103771f2c0a174cf
+
 ]
