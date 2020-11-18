4c8732727391ba487a9e19fe2439114b2b16ed6c
Sungmin Woo <woos8899@gmail.com>
2020-11-19 00:40:24 +0900

    Updated to support latest submodule commit (#9)
    * Use latest main branch
    
    * Updated db creation functions
    
    * Changed a function call
    
    * Use strain instead of acc_id for test
    
    * Update DGU2020_covid_database
    
    * Update readme.md
    
    * Use strain key in test

--- webapi/management/commands/gen_blast_db.py
+++ webapi/management/commands/gen_blast_db.py
@@ -8,20 +8,20 @@ import dgucovidb.db_interface as bst
 class Command(BaseCommand):
     help = 'Build BLAST database using a fasta file'
 
-    __ARG_SRG_FASTA = "src_fasta"
+    __ARG_DB_SRC_PATH = "db_src_folder_path"
 
     def add_arguments(self, parser):
-        parser.add_argument(self.__ARG_SRG_FASTA, type=str)
+        parser.add_argument(self.__ARG_DB_SRC_PATH, type=str)
 
     def handle(self, *args, **options):
-        fasta_path = str(options[self.__ARG_SRG_FASTA])
-        if not os.path.isfile(fasta_path):
-            raise CommandError("'{}' is not a file".format(fasta_path))
+        db_src_fol_path = str(options[self.__ARG_DB_SRC_PATH])
+        if not os.path.isdir(db_src_fol_path):
+            raise CommandError("'{}' is not a valid folder".format(db_src_fol_path))
 
         blast = bst.InterfBLAST("./database/blast")
 
         try:
-            blast.create_db(fasta_path)
+            blast.create_db(os.path.join(db_src_fol_path, "sequences.fasta"))
         except FileExistsError:
             self.stdout.write("blast db aleady exists")
         else:
